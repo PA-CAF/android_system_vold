1847a16ada19c3d89ba91eff23f931a24ce70ee6
Tom Marshall <tdm@cyngn.com>
2015-11-04 15:35:09 -0800

    vold: minivold for recovery
     * Build static linked minivold
     * Adjust tool paths for recovery
     * Skip ASEC and FUSE in recovery
     * Default volume label to fstab label
     * Add label "emulated" to emulated volume
     * Always send VolumeFsLabelChanged on volume creation
     * Add detach option to volume unmount
    
    Change-Id: I1a943dd3e606caa6bee6a0ec1df54fed365f769a

--- VolumeBase.cpp
+++ VolumeBase.cpp
@@ -166,9 +166,9 @@ status_t VolumeBase::create() {
     }
 
     mCreated = true;
-    status_t res = doCreate();
     notifyEvent(ResponseCode::VolumeCreated,
             StringPrintf("%d \"%s\" \"%s\"", mType, mDiskId.c_str(), mPartGuid.c_str()));
+    status_t res = doCreate();
     setState(State::kUnmounted);
     return res;
 }
@@ -216,7 +216,7 @@ status_t VolumeBase::mount() {
     return res;
 }
 
-status_t VolumeBase::unmount() {
+status_t VolumeBase::unmount(bool detach /* = false */) {
     if (mState != State::kMounted) {
         LOG(WARNING) << getId() << " unmount requires state mounted";
         return -EBUSY;
@@ -231,7 +231,7 @@ status_t VolumeBase::unmount() {
     }
     mVolumes.clear();
 
-    status_t res = doUnmount();
+    status_t res = doUnmount(detach);
     setState(State::kUnmounted);
     return res;
 }
